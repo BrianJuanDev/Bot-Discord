ad77032df7e5f646c1efe64438569aa9cdec61d6
Brian <Brianjuandev@gmail.com>
2024-04-07 22:57:28 -0300

    added step-0

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 from typing import Final
+import os
+from dotenv import load_dotenv
+from discord import Intents, Client, Message
+from response import get_response
+
+# STEP 0: LOAD OUR TOKEN FROM SOMEWHERE SAFE
+load_dotenv()
+TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
