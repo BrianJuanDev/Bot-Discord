6c09da1d8f4928606ed16ad2d37e201c0c47b5ec
Brian <Brianjuandev@gmail.com>
2024-04-08 00:05:54 -0300

    added step-2

--- main.py
+++ main.py
@@ -18,5 +18,13 @@ async def send_message(message: Message, user_message: str)-> None:
     if not user_message:
         print('Message not found')
         return
+    
     if is_private := user_message[0] == '?':
-        return
+        user_message = user_message[1:]
+
+    try:
+        response: str = get_response(user_message)
+        await message.author.send(response) if is_private else message.channel.send(response)
+
+    except Exception as e:
+        print(e)
