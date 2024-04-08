65b4cf8d86ce11a59b7d8506c8eb4e17f61c5649
Brian <Brianjuandev@gmail.com>
2024-04-07 23:43:24 -0300

    added step-1

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ from response import get_response
 # STEP 0: LOAD OUR TOKEN FROM SOMEWHERE SAFE
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
+
+# STEP 1: BOT SETUP
+intents: Intents = Intents.default()
+intents.message_content = True #NOQA
+client: Client = Client(intents=intents)
+
+# STEP 2: MESSAGE FUNCIONALITY
+async def send_message(message: Message, user_message: str)-> None:
+    if not user_message:
+        print('Message not found')
+        return
+    if is_private := user_message[0] == '?':
+        return
