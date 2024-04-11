323be332737d049a029b771fc11aa3df42acaf61
Brian <Brianjuandev@gmail.com>
2024-04-10 21:12:02 -0300

    added step-3

--- response.py
+++ response.py
@@ -1,2 +1,20 @@
+from random import choice, randint
+
+
 def get_response(user_input: str) -> str:
-    raise NotImplementedError('Code is missing..')
+    lowered: str = user_input.lower()
+
+    if lowered == '':
+        return 'Silent'
+    elif 'hello' in lowered:
+        return 'Hello there!'
+    elif 'how are you' in lowered:
+        return 'Good, thanks!'
+    elif 'bye' in lowered:
+        return 'See you!'
+    elif 'rol dice' in lowered:
+        return f'You rolled: {randint(1,6)}'
+    else:
+        return choice(['I do not understand...',
+                       'What are you talking about',
+                       'Do you mind rephasing that?'])
