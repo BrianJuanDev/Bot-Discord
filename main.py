5b477094ef8b25d403f6120453eff4890d14ce7a
Brian <Brianjuandev@gmail.com>
2024-04-12 20:38:12 -0300

    added step-4 handling incoming messages

--- main.py
+++ main.py
@@ -30,6 +30,18 @@ async def send_message(message: Message, user_message: str)-> None:
         print(e)
 
 # STEP 3: HANDLING THE STARTUP FOR OUR BOT
-@Client.event
+@client.event
 async def on_ready() -> None:
     print(f'{client.user} is now running')
+
+# STEP 4: HANDLING INCOMING MESSAGES
+@client.event
+async def on_message(message: Message) -> None:
+    if message.author == client.user:
+        return
+    username: str = str(message.author)
+    user_message: str = str(message.content)
+    channel: str = str(message.channel)
+
+    print(f'[{channel}{username}:"{user_message}"]')
+    await send_message(message, user_message)
